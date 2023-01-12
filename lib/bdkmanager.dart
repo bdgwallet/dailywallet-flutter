4f3b6146dee608bc6f7a938757de064f5536299f
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2023-01-12 17:50:47 +0000

    WIP, stub bdkmanager as changenotifier

--- lib/bdkmanager.dart
+++ lib/bdkmanager.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:bdk_flutter/bdk_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class BDKManager extends ChangeNotifier {
+  BDKManager(this.databaseConfig, this.blockchainConfig);
+
+  Wallet? wallet;
+  Balance? balance;
+  var transactions = <TransactionDetails>[];
+  var syncState = SyncState.empty;
+
+  final DatabaseConfig databaseConfig;
+  final BlockchainConfig blockchainConfig;
+
+  Future<void> signOut() {
+    // update state
+    user = null;
+    // and notify any listeners
+    notifyListeners();
+  }
+}
+
+final bdkManagerProvider = ChangeNotifierProvider<BDKManager>((ref) {
+  return BDKManager();
+});
+
+enum SyncState { empty, syncing, synced, failed }
