cbff2de106b0e33bbcb1134b56327e0f8ae634ec
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2023-01-13 18:07:35 +0000

    Tweak

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -132,7 +132,7 @@ class SettingsScreen extends ConsumerWidget {
           appBar: AppBar(
             backgroundColor: Colors.white,
             elevation: 0.0,
-            title: Center(
+            title: const Center(
               child: Text("Settings"),
             ),
           ),
