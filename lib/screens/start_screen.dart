cafe32dfee3363bae7bda6ba9b5008c3b24e7455
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2023-06-02 12:03:09 +0100

    Rename to StartScreen

---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'package:bdk_flutter/bdk_flutter.dart';
+import 'package:bitcoin_ui_kit/bitcoin_ui_kit.dart';
+import 'package:dailywallet_flutter/ldknode_manager.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:dailywallet_flutter/key_manager.dart';
+
+class StartScreen extends ConsumerWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ldkNodeManager = ref.watch(ldkNodeManagerProvider);
+
+    return Container(
+        margin: const EdgeInsets.fromLTRB(32, 64, 32, 32),
+        child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Spacer(),
+          Image(
+              image: const AssetImage("icons/bitcoin_circle.png",
+                  package: "bitcoin_ui_kit"),
+              height: 125,
+              fit: BoxFit.fill,
+              color: Bitcoin.orange),
+          const SizedBox(height: 16),
+          Text("Bitcoin Wallet", style: BitcoinTextStyle.title1(Bitcoin.black)),
+          const SizedBox(height: 16),
+          Text(
+            "A simple bitcoin wallet for your enjoyment",
+            style: BitcoinTextStyle.body1(Bitcoin.neutral7),
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+          BitcoinButtonFilled(
+            title: "Create wallet",
+            onPressed: () {
+              Mnemonic.create(WordCount.Words12).then((mnemonic) {
+                final keydata = KeyData(mnemonic.asString());
+                saveKeyData(keydata);
+                ldkNodeManager.start(mnemonic.asString());
+              });
+            },
+          ),
+          const SizedBox(height: 16),
+          BitcoinButtonPlain(
+            title: "Restore wallet",
+            onPressed: () {
+              // TODO: Restore flow
+              debugPrint("Restore wallet: Not yet implemented");
+            },
+          ),
+          const SizedBox(height: 16),
+          Text(
+            "Your wallet, your coins\n100% open-source & open-design",
+            style: BitcoinTextStyle.body4(Bitcoin.neutral7),
+            textAlign: TextAlign.center,
+          ),
+        ])));
+  }
+}
